5bca2a206f4b16f6029655f8c64d5859acf01941
Neo <neodilk@gmail.com>
2025-10-06 18:41:51 +0200

    Added Logo and Metadata

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -6,6 +6,31 @@ import { SeasonsSection } from "@/components/general/seasons";
 import { ModpackSection } from "@/components/general/modpack";
 import { GallerySlider } from "@/components/general/gallery";
 
+export const metadata = {
+    title: "Brassworks – Create SMP",
+    description: "Our public server thrives on cooperation between players - express your creativity freely, with each other.",
+    openGraph: {
+        title: "Brassworks – Create SMP",
+        description: "Our public server thrives on cooperation between players - express your creativity freely, with each other.",
+        url: "https://brassworks.572.at/",
+        type: "website",
+        images: [
+            {
+                url: "https://brassworks.572.at/images/logo.png",
+                width: 1200,
+                height: 630,
+                alt: "Brassworks Cover",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Brassworks – Create SMP",
+        description: "Our public server thrives on cooperation between players - express your creativity freely, with each other.",
+        images: ["https://brassworks.572.at/images/logo.png"],
+    },
+};
+
 export default function Home() {
   return (
     <div className="relative mx-auto">
